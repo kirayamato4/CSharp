3b7144b8a33a97bd563895daa4d4da31c0df3f66
Dante <kirayamato4@naver.com>
2019-02-01 21:30:49 +0900

    Chapter02 문자 입출력 예제
    문자 입출력 에제

---
+++ Practice/Chapter02/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter02
+{
+	public partial class Form1 : Form
+	{
+		// 결과 : 저장용 변수
+		string result = "";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			result = this.lbResult.Text;
+		}
+
+		private void btnEdit_Click(object sender, EventArgs e)
+		{
+			if( TextCheck() )
+			{
+				this.lbResult.Text = result + this.txtEdit.Text;
+			}
+		}
+
+		private bool TextCheck()
+		{
+			if ("" != this.txtEdit.Text)
+			{
+				return true;
+			}
+			else
+			{
+				MessageBox.Show("텍스트를 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				this.txtEdit.Focus();
+				return false;
+			}
+		}
+
+		private void txtEdit_KeyPress( object sender, KeyPressEventArgs e )
+		{
+			// 13: 엔터키
+			if( e.KeyChar == (char)13 )
+			{
+				// 소리 없애기
+				e.Handled = true;
+
+				if( TextCheck() )
+				{
+					this.lbResult.Text = result + this.txtEdit.Text;
+				}
+			}
+		}
+	}
+}
